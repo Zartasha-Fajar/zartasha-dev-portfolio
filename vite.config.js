2bdccebb10713bafad35c830acbf560b6b8a8aae
Zartasha-Fajar <zartasha.fajar99@gmail.com>
2025-05-30 11:10:48 +0500

    Added Changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // Replace `Zartasha-Portfolio` with your **repo name**
 export default defineConfig({
-  base: 'Zartasha-Portfolio',
   plugins: [react()],
 })
